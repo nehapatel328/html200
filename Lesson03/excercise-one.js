7ae98ddd5d9b5b4f36e5522929d2b01f49555261
Neha Patel <nehapatel0393@gmail.com>
2019-04-19 23:10:31 -0500

    push, splice

---
+++ Lesson03/excercise-one.js
@@ -0,0 +1,10 @@
+let brunchFood = ['mimosa', 'avocado toast', 'eggs benedict', 'gluten free waffles', 'Bloody Mary'];
+console.log(arr[2]);
+
+console.log(arr);
+
+brunchFood.push ('strawberry pancakes');
+console.log(brunchFood);
+
+brunchFood.splice(3, 1);
+console.log(brunchFood);
