aed504d707c6c216e79433aa68217789bad81af2
Neha Patel <nehapatel0393@gmail.com>
2019-04-22 23:04:35 -0500

    First commit, check that arr is working

---
+++ Lesson03/lesson-03-bonus.js
@@ -0,0 +1,23 @@
+let arr = [['Heart', 'Diamond', 'Club', 'Spade'], ['Ace', 'King', 'Queen', 'Jack','One', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine' ]];
+console.log(arr[0]);
+//["Heart", "Diamond", "Club", "Spade"]
+
+console.log(arr[1]);
+//["Ace", "King", "Queen", "Jack", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine"]
+
+console.log(arr[0][1]);
+//"Diamond"
+
+console.log(arr[1][1]);
+//"King"
+
+console.log(arr.length);
+//2
+
+console.log(arr[0].length);
+//4
+
+console.log(arr[1].length);
+//13
+
+//Check that arr is working
