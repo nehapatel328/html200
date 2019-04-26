8ff4cf75984b2f5c37ef359884fe42c753e1044d
Neha Patel <nehapatel0393@gmail.com>
2019-04-25 23:54:13 -0500

    first commit

--- Lesson03/lesson-03-bonus.js
+++ Lesson03/lesson-03-bonus.js
@@ -21,3 +21,34 @@ console.log(arr[1].length);
 //13
 
 //Check that arr is working
+
+for (let i = 0; i < arr.length; i++) {
+  console.log(arr);
+
+  for (let j = 0; j < arr[i].length; j++) {
+    console.log(arr[i][j]);
+  }
+}
+
+/* Check that array within array is working
+[["Heart", "Diamond", "Club", "Spade"], ["Ace", "King", "Queen", "Jack", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine"]]
+"Heart"
+"Diamond"
+"Club"
+"Spade"
+
+[["Heart", "Diamond", "Club", "Spade"], ["Ace", "King", "Queen", "Jack", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine"]]
+"Ace"
+"King"
+"Queen"
+"Jack"
+"One"
+"Two"
+"Three"
+"Four"
+"Five"
+"Six"
+"Seven"
+"Eight"
+"Nine"
+*/
---
+++ Lesson04/demo.js
@@ -0,0 +1,11 @@
+console.log('JavaScript is working!');
+
+//See numberAdder on html page.
+function numberAdder(firstNumber) {
+  let input = prompt('Enter a number');
+  let numberToAdd = 2;
+  let result = 0;
+
+  result = numberToAdd + Number(input);
+
+}
