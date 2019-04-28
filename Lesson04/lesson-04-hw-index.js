53952aa52142c035b05310fa50d05d63cb5e7f86
Neha Patel <nehapatel0393@gmail.com>
2019-04-28 15:38:36 -0500

    formatting

--- Lesson04/lesson-04-hw-index.js
+++ Lesson04/lesson-04-hw-index.js
@@ -1,9 +1,9 @@
 function squareRoot() {
 let number = prompt('Enter a number: ') //alert to screen
 
-function squareRoot(numberToSquare) { //callback function with a name of 'square'
-  return Math.pow(numberToSquare,2); //returns square of a number with a variable of 'numberToSquare' or what the user inputs
-}
+  function squareRoot(numberToSquare) { //callback function with a name of 'square'
+    return Math.pow(numberToSquare,2); //returns square of a number with a variable of 'numberToSquare' or what the user inputs
+  }
 
 alert(squareRoot(number)); //print alert to the screen with the user input squared
 
