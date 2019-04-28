60770891c3e0782774e7fb1ec20c641ad0b226ee
Neha Patel <nehapatel0393@gmail.com>
2019-04-28 14:56:33 -0500

    Kept functions separate. Button still won't work on click.

--- Lesson04/lesson-04-hw-index.js
+++ Lesson04/lesson-04-hw-index.js
@@ -1,9 +1,9 @@
 
-let number = prompt ('Enter a number') //alert to screen
+let number = prompt('Enter a number: ') //alert to screen
 
-function squareRoot (numberToSquare) { //callback function with a name of 'square'
-  numberToSquare = Math.sqrt(numberToSquare); //returns square root of a number with a variable of 'numberToSquare' or what the user inputs
-  alert(numberToSquare); //print alert to the screen with the user input squared
+function squareRoot(numberToSquare) { //callback function with a name of 'square'
+  return Math.sqrt(numberToSquare); //returns square root of a number with a variable of 'numberToSquare' or what the user inputs
 }
+alert(squareRoot(number)); //print alert to the screen with the user input squared
 
-squareRoot(number);
+//new version keeps functions separate 
