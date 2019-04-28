331092029c6f309e994ef6fbf7cf7691d1debbba
Neha Patel <nehapatel0393@gmail.com>
2019-04-28 15:38:03 -0500

    Got alerts to appear on button click

--- Lesson04/lesson-04-hw-index.js
+++ Lesson04/lesson-04-hw-index.js
@@ -1,9 +1,11 @@
-
+function squareRoot() {
 let number = prompt('Enter a number: ') //alert to screen
 
 function squareRoot(numberToSquare) { //callback function with a name of 'square'
-  return Math.sqrt(numberToSquare); //returns square root of a number with a variable of 'numberToSquare' or what the user inputs
+  return Math.pow(numberToSquare,2); //returns square of a number with a variable of 'numberToSquare' or what the user inputs
 }
+
 alert(squareRoot(number)); //print alert to the screen with the user input squared
 
-//new version keeps functions separate 
+//new version keeps functions separate
+}
